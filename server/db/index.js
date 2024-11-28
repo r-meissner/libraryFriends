8eca8aeae2adaef1187c39414f1f1543137c0463
Parvin <parvin.mageswararao@agilimo.de>
2024-11-28 20:15:20 +0100

    Implemented MongoDB configuration

--- server/db/index.js
+++ server/db/index.js
@@ -1,1 +1,17 @@
-//placeholder
+import { MongoClient } from 'mongodb';
+
+const client = new MongoClient(process.env.MONGO_URI);
+
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  }
+}
+
+connectDB();
+
+export default client;
