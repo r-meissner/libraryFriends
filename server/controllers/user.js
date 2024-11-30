6d07b2796119871acf86da12cc481159868f4338
Ruth Meissner <ruthmeissner96@gmail.com>
2024-11-30 15:51:21 +0100

    deleted update user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -9,21 +9,6 @@ export const getUserById = asyncHandler(async (req, res) => {
   res.status(200).json(user);
 });
 
-export const updateUser = asyncHandler(async (req, res) => {
-  const { id } = req.params;
-  const { userName, email, password, city, country, avatar } = req.body;
-  if (!userName || !email)
-    throw new ErrorResponse("Username and email are required", 400);
-  let updateFields = { userName, email, city, country, avatar };
-  if (password) {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    updateFields.password = hashedPassword;
-  }
-  const user = await User.findByIdAndUpdate(id, updateFields, { new: true });
-  if (!user) throw new ErrorResponse("User not found", 404);
-  res.status(200).json(user);
-});
-
 export const searchUserByEmail = asyncHandler(async (req, res) => {
   const { email } = req.query;
   if (!email) throw new ErrorResponse("Email is required", 400);
