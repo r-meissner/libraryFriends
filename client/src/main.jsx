d98620ec5f4fbaaa6340fad906c28fa7cc64d2fb
Lisa <helbig-lisa@web.de>
2024-12-02 18:59:31 +0100

    protected routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,19 @@ import ErrorPage from './pages/ErrorPage.jsx';
 import HomePage from './pages/HomePage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import SignUp from './pages/SignUpPage.jsx';
+import MyFriendsPage from './pages/MyFriendsPage.jsx';
+import SharedLibraryPage from './pages/SharedLibraryPage.jsx';
+import MyBooksPage from './pages/MyBooksPage.jsx';
+import MyBorrowedBooksPage from './pages/MyBorrowedBooksPage.jsx';
+import MyLentBooksPage from './pages/MyLentBooksPage.jsx';
+import PublicUserProfile from './pages/PublicUserProfile.jsx';
+import ProfileSettingsPage from './pages/ProfileSettingsPage.jsx';
+import BookDetailPage from './pages/BookDetailPage.jsx';
+import ProtectedRoute from './layouts/ProtectedRoute.jsx';
+import AuthProvider from './context/AuthContextProvider.jsx';
+
+
+
 
 const router = createBrowserRouter([
   {
@@ -26,6 +39,70 @@ const router = createBrowserRouter([
         path: '/signup',
         element: <SignUp />,
       },
+      {
+        path: '/friends',
+        element: (
+          <ProtectedRoute>
+            <MyFriendsPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/sharedlibrary',
+        element: (
+          <ProtectedRoute>
+            <SharedLibraryPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/mybooks',
+        element: (
+          <ProtectedRoute>
+            <MyBooksPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/myborrowedbooks',
+        element: (
+          <ProtectedRoute>
+            <MyBorrowedBooksPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/mylentbooks',
+        element: (
+          <ProtectedRoute>
+            <MyLentBooksPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/profile/:userid',
+        element: (
+          <ProtectedRoute>
+            <PublicUserProfile />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/settings/:userid',
+        element: (
+          <ProtectedRoute>
+            <ProfileSettingsPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/book/:bookid',
+        element: (
+          <ProtectedRoute>
+            <BookDetailPage />
+          </ProtectedRoute>
+        ),
+      }
     ],
   },
 ]);
