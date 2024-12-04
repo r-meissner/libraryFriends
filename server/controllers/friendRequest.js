07b13cbb655f25fc8b891e7c098c1e5bc7938fec
Ruth Meissner <ruthmeissner96@gmail.com>
2024-12-04 20:24:59 +0100

    testing and fixes

--- server/controllers/friendRequest.js
+++ server/controllers/friendRequest.js
@@ -4,8 +4,8 @@ import ErrorResponse from "../utils/ErrorResponse.js";
 
 export const getFriendRequestsOfUser = asyncHandler(async (req, res) => {
     const { userId } = req.body;
-    const sentRequests = await FriendRequest.find({requestingUser: userId, status: 'open'}).populate('targetUser');
-    const receivedRequests = await FriendRequest.find({targetUser: userId, status: 'open'}).populate('requestingUser');
+    const sentRequests = await FriendRequest.find({requestingUser: userId, status: 'open'}).populate('targetUser','userName email avatar');
+    const receivedRequests = await FriendRequest.find({targetUser: userId, status: 'open'}).populate('requestingUser', 'userName email avatar');
     res.status(200).json({
         sentRequests: sentRequests,
         receivedRequests: receivedRequests,
@@ -23,5 +23,8 @@ export const createFriendRequest = asyncHandler(async (req, res) => {
 });
 
 export const deleteFriendRequest = asyncHandler(async (req, res) => {
-    
-})
+    const { id } = req.params;
+    const friendRequest = await FriendRequest.findByIdAndDelete(id);
+    if (!friendRequest) throw new ErrorResponse('Friend request not found', 404);
+    res.status(200).json({ message: 'Friend request deleted' });
+  });
